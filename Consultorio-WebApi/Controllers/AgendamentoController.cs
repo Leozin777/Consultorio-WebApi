f7a6ab41eaca22d4fb40b4a0cf0f41a832871484
leozin777 <leo.cardoso@rede.ulbra.br>
2022-07-19 17:34:28 -0300

    Initial Commit

---
+++ Consultorio-WebApi/Controllers/AgendamentoController.cs
@@ -0,0 +1,40 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        List<Agendamento> agendamentos = new List<Agendamento>();
+
+        public AgendamentoController()
+        {
+            agendamentos.Add(new Agendamento
+            {
+                Id = 1,
+                NomePaciente = "Leo",
+                Horario = new DateTime(2022, 07, 18)
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(agendamentos);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int id)
+        {
+            var agendamentoSelecionado = agendamentos.FirstOrDefault(x => x.Id == id);
+
+            if (agendamentoSelecionado != null)
+                return Ok(agendamentoSelecionado);
+            else
+                return BadRequest("não existe");
+        }
+    }
+}
+
